223b62f6d2a1d75d3d69a789132b16c5b846b338
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-01 14:51:20 -0400

    first commit on part 6

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Part 5
+// Select the section id container and change the text to "Hello!" 
 const section = document.getElementById("container");
 section.innerText = "Hello!";
+// Part 6
+// Select the div with the class of footer and add the class main to it
+const footer = document.getElementById("footer");
+footer.classList.add("main");
