260dfa8190180b0a8573a8175a039888d5af61b5
Alan Eappen <eappenalan123@gmail.com>
2020-06-01 15:15:13 -0400

    final solution

---
+++ script.js
@@ -0,0 +1,7 @@
+var makeBlue = document.getElementById("container").childNodes[3].classList.add("makeBlue");
+
+var removeDiv = document.getElementsByClassName("footer")[0];
+
+removeDiv.remove();
+
+
